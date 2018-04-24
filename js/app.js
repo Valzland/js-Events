c6fe21934b120ba7df8de2c036165ea65508493e
velzland <valzey.freitas@gmail.com>
2018-04-23 19:45:23 -1000

    almost there

--- js/app.js
+++ js/app.js
@@ -6,30 +6,58 @@ The function will add the following content inside the paragraph with the id of 
 
 var bacon = "Shoulder turducken brisket, kevin swine andouille tri-tip salami tail ham sausage pork loin. Ribeye short loin rump kielbasa pork. Capicola short loin turducken corned beef tongue, chuck leberkas salami frankfurter. Kielbasa fatback pancetta, ground round meatball turducken jowl ribeye alcatra sirloin bacon corned beef beef ribs short loin. Pork belly spare ribs biltong corned beef meatball short ribs tongue alcatra swine drumstick. Biltong shankle kevin, cupim sirloin bresaola brisket. Tail pork belly biltong ball tip tri-tip, pig jerky cow pastrami prosciutto ;ground round bacon capicola tongue meatball.";
 
+function moreContent(){
+    var pElem = document.getElementById("more");
+    pElem.innerHTML = bacon;
+}
 
 //2. HTTP
 /*Create a function named `lessContent` that will initiate the `Show Less` link after clicking on it.
 
 The function will hide the contents in the pargraph with the id of `less` after clicking on the `Show Less` link.*/
 
+function lessContent (){
+//console.log ("test")
+var pElem = document.getElementById("less");
+pElem.innerHTML = "";
+}
 
 //3. Tacocat, The Original Palindrome King
 /*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increae the font size to 150%*/
 
-
+function zoom (){
+var biggie = document.getElementById("biggie");
+biggie.style.fontSize = "150%";
+}
 //4. McDonalds
 /*Create a variable name menuItems and assign it an array of three of your favorite items at McDonald's.
 
 Next, create a function named valueMenu that will display your favorite items in the paragraph the the id of `menu` after clicking on the showMenu paragraph.*/
 
+var menuItems = ['Filet Fish', ' Chocolate Shake', ' Small Fries'];
+
+showMenu.addEventListener('click', valueMenu);
+function valueMenu (){
+menu.innerHTML = menuItems
+}
 
 //5. Gin.
 /*Create a function named redFace that will change the paragraph text to red and a font size of 30px after clicking on the text.*/
+drink.addEventListener("click", redFace)
+function redFace (){
+drink.style.color = 'red';
+drink.style.fontSize = '30px'
 
-
+}
 //6. Peanut Butter Cup Oreos
 /*Create a function `showPrice` that will add the price of `$5.55` inside the paragraph with the id `price` after hovering your mouse over the paragraph.*/
 
+onmouseover.addEventListener(showPrice);
+
+function showPrice (){
+    var price = getElementById("price");
+    price.innerHTML= '5.55';
+}
 
 //7. Mr. Buttons
 /*Add an Event Listener to the button that will display `myQuote` inside the paragraph with the id of `displayQuote` after the button is clicked.*/
